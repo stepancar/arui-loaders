a78e5d6a5bb216b4f204aae3fcaa5a323dd4ea06
stepancar <stepancar@hotmail.com>
2017-11-08 23:21:39 +0300

    chore(*): icon-loader draft

--- src/arui-icon-loader.js
+++ src/arui-icon-loader.js
@@ -1,10 +1,11 @@
 export default function(source) {
     const options = getOptions(this);
     // main idea:
-    // find Icon component in sources
+    // find Icon component in sources (simple regexp/parse ast?)
     // get props, passed to component (icon, color, size)
     // build path to icon
     // insert smthlike <img src={require('pathToIcon') />
+    // add pathToIcon to dependencies for watch mode
     if (options.cb) {
         options.cb(source);
     }
